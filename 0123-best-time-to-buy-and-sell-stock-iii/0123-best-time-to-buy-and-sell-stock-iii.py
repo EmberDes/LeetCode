7449e17b7f91ac8b90fbc478886196544a3807c6
EmberDes <shashwatsinghdit@gmail.com>
2024-06-16 23:04:51 +0530

    Time: 563 ms (99.83%), Space: 31.3 MB (43.68%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,23 +1,34 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+        
+#         dp = {}
+        
+#         def dfs(i,buying,cap) :
+#             if i >= len(prices) or cap>2:
+#                 return 0
+#             if (i,buying,cap) in dp:
+#                 return dp[(i,buying,cap)]
+            
+#             if buying:
+#                 buy = dfs(i+1,not buying,cap+1)-prices[i]
+#                 cooldown = dfs(i+1, buying,cap)
+#                 dp[(i,buying,cap)] = max(buy,cooldown)
+#             else:
+#                 sell = dfs(i+1,not buying,cap) + prices[i]
+#                 cooldown = dfs(i+1, buying,cap)
+#                 dp[(i,buying,cap)] = max(sell,cooldown)
+            
+#             return dp[(i,buying,cap)]
+        
+#         return dfs(0,True,0)
+
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        
-        dp = {}
-        
-        def dfs(i,buying,cap) :
-            if i >= len(prices) or cap>2:
-                return 0
-            if (i,buying,cap) in dp:
-                return dp[(i,buying,cap)]
-            
-            if buying:
-                buy = dfs(i+1,not buying,cap+1)-prices[i]
-                cooldown = dfs(i+1, buying,cap)
-                dp[(i,buying,cap)] = max(buy,cooldown)
-            else:
-                sell = dfs(i+1,not buying,cap) + prices[i]
-                cooldown = dfs(i+1, buying,cap)
-                dp[(i,buying,cap)] = max(sell,cooldown)
-            
-            return dp[(i,buying,cap)]
-        
-        return dfs(0,True,0)
+    def maxProfit(self, prices):
+        s1 = s2 = 0
+        b1 = b2 = -float("inf")
+        for p in prices:
+            if -p > b1: b1 = -p
+            if b1 + p > s1: s1 = b1 + p
+            if s1 - p > b2: b2 = s1 - p
+            if b2 + p > s2: s2 = b2 + p
+        return s2
