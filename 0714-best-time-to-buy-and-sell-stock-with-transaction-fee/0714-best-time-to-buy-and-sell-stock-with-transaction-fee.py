e93271561d8dd76c7a10a44e83d7155961da692d
EmberDes <shashwatsinghdit@gmail.com>
2024-06-16 22:16:20 +0530

    Time: 423 ms (99.23%), Space: 23.4 MB (98.30%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,25 +1,50 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int], fee: int) -> int:
+        
+        
+#         dp = {}
+        
+#         def dfs(i,buying) :
+#             if i >= len(prices):
+#                 return 0
+#             if (i,buying) in dp:
+#                 return dp[(i,buying)]
+            
+#             if buying:
+#                 buy = dfs(i+1,not buying)-prices[i]
+#                 cooldown = dfs(i+1, buying)
+#                 dp[(i,buying)] = max(buy,cooldown)
+#             else:
+#                 sell = dfs(i+1,not buying) + prices[i]-fee
+#                 cooldown = dfs(i+1, buying)
+#                 dp[(i,buying)] = max(sell,cooldown)
+            
+#             return dp[(i,buying)]
+        
+#         return dfs(0,True)
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         
+        # Initialize the minimum price with the first stock price plus fee
+        min_price = prices[0] + fee
+        # Initialize the maximum profit as 0
+        max_profit = 0
         
-        dp = {}
+        # Iterate over each stock price
+        for price in prices:
+            # If the current price is greater than the minimum price,
+            # a profit can be made by selling the stock
+            if min_price < price:
+                # Add the difference between the current price and the minimum price
+                # to the maximum profit
+                max_profit += price - min_price
+                # Update the minimum price to the current price
+                min_price = price
+            # If the current price plus fee is less than the minimum price,
+            # update the minimum price to the current price plus fee
+            elif price + fee < min_price:
+                min_price = price + fee
         
-        def dfs(i,buying) :
-            if i >= len(prices):
-                return 0
-            if (i,buying) in dp:
-                return dp[(i,buying)]
-            
-            if buying:
-                buy = dfs(i+1,not buying)-prices[i]
-                cooldown = dfs(i+1, buying)
-                dp[(i,buying)] = max(buy,cooldown)
-            else:
-                sell = dfs(i+1,not buying) + prices[i]-fee
-                cooldown = dfs(i+1, buying)
-                dp[(i,buying)] = max(sell,cooldown)
-            
-            return dp[(i,buying)]
-        
-        return dfs(0,True)
+        # Return the maximum profit
+        return max_profit
         
