7079c72bc5275923358da08424a8a02430217bac
EmberDes <shashwatsinghdit@gmail.com>
2026-02-25 14:46:07 +0530

    Time: 1061 ms (11.22%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,6 +1,11 @@
-select u.name,ifnull(sum(r.distance),0) as travelled_distance
-from users as u
-left join rides as r
-on u.id=r.user_id
-group by u.id
-order by travelled_distance desc,name; 
+-- select u.name,ifnull(sum(r.distance),0) as travelled_distance
+-- from users  u
+-- left join rides  r
+-- on u.id=r.user_id
+-- group by u.id
+-- order by travelled_distance desc,name; 
+SELECT name AS name, IFNULL(SUM(distance),0) AS travelled_distance 
+FROM Users u LEFT JOIN Rides r 
+ON u.id = r.user_id 
+GROUP BY u.id 
+ORDER BY travelled_distance DESC, u.name ASC; 
