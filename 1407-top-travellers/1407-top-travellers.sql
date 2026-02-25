92d83a50fc5c5b97d1a41ae176fb7d05bb4a2685
EmberDes <shashwatsinghdit@gmail.com>
2026-02-25 14:44:44 +0530

    Time: 780 ms (73.01%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,4 +1,6 @@
-select u.name,  COALESCE(SUM(distance), 0) as travelled_distance from Users u  left join Rides r on u.id = r.user_id
- group by u.id,u.name order by
-
-travelled_distance desc ,u.name asc
+select u.name,ifnull(sum(r.distance),0) as travelled_distance
+from users as u
+left join rides as r
+on u.id=r.user_id
+group by u.id
+order by travelled_distance desc,name; 
